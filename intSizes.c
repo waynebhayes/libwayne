076c51eb72745ce628b7a6ffc315f074e8656710
Wayne Hayes <whayes@uci.edu>
2024-04-25 11:43:02 -0700

    make __int128 conditional on its existence in intSize.c

--- intSizes.c
+++ intSizes.c
@@ -8,7 +8,7 @@ char *space2line(const char *S) {
     return t;
 }
 
-#define PrintSz(x) printf("#define sizeof_%s %ld\n#define %s_width %ld\n", space2line(#x), sizeof(x), space2line(#x), 8*sizeof(x))
+#define PrintSz(x) printf("#define sizeof_%s %d\n#define %s_width %d\n", space2line(#x), sizeof(x), space2line(#x), 8*sizeof(x))
 
 int main(void) {
     unsigned char c=1, nBits=1;
@@ -19,6 +19,8 @@ int main(void) {
     PrintSz(int);
     PrintSz(long);
     PrintSz(long long);
+#if __GLIBCXX_TYPE_INT_N_0 == 128
     PrintSz(__int128);
+#endif
     return 0;
 }
