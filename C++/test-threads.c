291b88de0dc9786ad80e4b51b99c886b0c85aa01
Wayne B. Hayes <whayes@uci.edu>
2024-06-17 16:45:21 -0700

    try shared memory access

--- C++/test-threads.c
+++ C++/test-threads.c
@@ -11,10 +11,15 @@
 #define COUNT_MOD 1000000000U
 
 foint ThreadTest(const foint f) {
+    static char _race[COUNT+1]; // who got there first?
     assert(f.i < NUM_THREADS);
-    printf(" Starting thread %d", f.i);
+    printf(" Starting thread %d", f.i+1);
     unsigned i;
-    for(i=0; i<COUNT;i++) if(i % COUNT_MOD == 0) {printf(" %d:%uM", f.i,i/1000000); fflush(stdout);}
+    for(i=0; i<=COUNT;i++) if(i % COUNT_MOD == 0) {
+	if(!_race[i]) {_race[i]=f.i+1; printf(" T%d:%uM!", f.i+1,i/1000000);}
+	else printf(" T%d:%u=%d", f.i+1,i/1000000,_race[i]);
+	fflush(stdout);
+    }
     printf("\n");
     return f;
 }
