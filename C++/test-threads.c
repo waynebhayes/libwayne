b20be0b7b87cb0b1034bf12db6fbc9fabebf734b
Wayne B. Hayes <whayes@uci.edu>
2024-08-14 09:09:22 +0200

    added real number summing test to thread test

--- C++/test-threads.c
+++ C++/test-threads.c
@@ -3,22 +3,35 @@
 #include <time.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include "rand48.h"
 #include "misc.h"
 #include "thread-sets.h"
+#include "mt19937.h"
 
+#define ORDINALS 100
 #define NUM_THREADS 4
-#define COUNT     1000000000U
-#define COUNT_MOD 100000000U
+#define COUNT     100000000U
+#define COUNT_MOD 10000000U
+
+static double _realSums[NUM_THREADS], _ordinalSums[ORDINALS];
+static MT19937 *_randGen[NUM_THREADS];
+
 
 foint ThreadTest(const foint f) {
     static char _race[COUNT+1]; // who got there first?
     assert(f.i < NUM_THREADS);
-    printf(" Starting thread %d", f.i+1);
+    printf("S%d", f.i+1);
     unsigned i;
-    for(i=0; i<=COUNT;i++) if(i % COUNT_MOD == 0) {
-	if(!_race[i]) {_race[i]=f.i+1; printf(" T%d:%uM!", f.i+1,i/1000000);}
-	else printf(" T%d:%u=%d", f.i+1,i/1000000,_race[i]);
-	fflush(stdout);
+    for(i=0; i<=COUNT;i++) {
+	double r = Mt19937NextDouble(_randGen[f.i]);
+	unsigned where = Mt19937NextDouble(_randGen[f.i])*ORDINALS;
+	_ordinalSums[where] += r;
+	_realSums[f.i] += r;
+	if(i % COUNT_MOD == 0) {
+	    if(!_race[i]) {_race[i]=f.i+1; printf(" T%d:%uM!", f.i+1,i/1000000);}
+	    else printf(" T%d:%u=%d", f.i+1,i/1000000,_race[i]);
+	    fflush(stdout);
+	}
     }
     printf("\n");
     return f;
@@ -27,9 +40,17 @@ foint ThreadTest(const foint f) {
 int main(){
     int i;
     foint threadInput[NUM_THREADS];
-    for(i=0;i<NUM_THREADS;i++) threadInput[i].i=i;
+    for(i=0;i<NUM_THREADS;i++) {
+ 	threadInput[i].i=i;
+	_randGen[i] = Mt19937Alloc(i);
+	double Mt19937NextDouble(const MT19937 *t);
+    }
     THREAD_SET *ts = ThreadSetAlloc(ThreadTest, NUM_THREADS, threadInput);
     ThreadSetRunAll(ts);
     ThreadSetFree(ts);
+    double realSum = 0.0, ordinalSum=0.0;
+    for(i=0; i<NUM_THREADS;i++) realSum += _realSums[i];
+    for(i=0; i<ORDINALS;i++) ordinalSum += _ordinalSums[i];
+    printf("\noverlapping sum %g, realSum %g, ratio %g\n", ordinalSum, realSum, ordinalSum/realSum);
     return 0;
 }
