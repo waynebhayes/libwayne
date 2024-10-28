e3c8a0d3e0f3029f11088607596b74efdf5fc14a
Wayne B. Hayes <whayes@uci.edu>
2024-10-28 16:53:14 -0700

    get number of CPUs using sysconf and set number of threads appropriately

--- C++/test-threads.c
+++ C++/test-threads.c
@@ -9,17 +9,17 @@
 #include "mt19937.h"
 
 #define ORDINALS 100
-#define NUM_THREADS 4
+#define MAX_THREADS 128
 #define COUNT     100000000U
 #define COUNT_MOD 10000000U
 
-static double _realSums[NUM_THREADS], _ordinalSums[ORDINALS];
-static MT19937 *_randGen[NUM_THREADS];
-
+static double _realSums[MAX_THREADS], _ordinalSums[ORDINALS];
+static MT19937 *_randGen[MAX_THREADS];
+static int _nCPUs;
 
 foint ThreadTest(const foint f) {
     static char _race[COUNT+1]; // who got there first?
-    assert(f.i < NUM_THREADS);
+    assert(f.i < _nCPUs);
     printf("S%d", f.i+1);
     unsigned i;
     for(i=0; i<=COUNT;i++) {
@@ -39,18 +39,20 @@ foint ThreadTest(const foint f) {
 
 int main(){
     int i;
-    foint threadInput[NUM_THREADS];
-    for(i=0;i<NUM_THREADS;i++) {
+    _nCPUs = sysconf(_SC_NPROCESSORS_ONLN);
+    assert(0 < _nCPUs && _nCPUs < MAX_THREADS);
+    foint threadInput[_nCPUs];
+    for(i=0;i<_nCPUs;i++) {
  	threadInput[i].i=i;
 	_randGen[i] = Mt19937Alloc(i);
 	double Mt19937NextDouble(const MT19937 *t);
     }
-    THREAD_SET *ts = ThreadSetAlloc(ThreadTest, NUM_THREADS, threadInput);
+    THREAD_SET *ts = ThreadSetAlloc(ThreadTest, _nCPUs, threadInput);
     ThreadSetRunAll(ts);
     ThreadSetFree(ts);
     double realSum = 0.0, ordinalSum=0.0;
-    for(i=0; i<NUM_THREADS;i++) realSum += _realSums[i];
+    for(i=0; i<_nCPUs;i++) realSum += _realSums[i];
     for(i=0; i<ORDINALS;i++) ordinalSum += _ordinalSums[i];
-    printf("\noverlapping sum %g, realSum %g, ratio %g\n", ordinalSum, realSum, ordinalSum/realSum);
+    printf("\noverlapping sum %g, realSum %g, error %g %%\n", ordinalSum, realSum, 100*(ordinalSum-realSum)/realSum);
     return 0;
 }
