ab199c81b1162a2185b3671b3b9d48baacb7ae77
Wayne Hayes <whayes@uci.edu>
2024-10-28 19:20:26 -0700

    more tests with threads

--- C++/test-threads.c
+++ C++/test-threads.c
@@ -3,37 +3,47 @@
 #include <time.h>
 #include <sys/types.h>
 #include <unistd.h>
-#include "rand48.h"
 #include "misc.h"
 #include "thread-sets.h"
-#include "mt19937.h"
 
-#define ORDINALS 100
+#define USE_MT19937 1 // if 0, use rand48() streams but it's WAY faster with C++ MT19937 (C's rand48() also isn't thread-safe)
+
+#define ORDINALS 1000
 #define MAX_THREADS 128
-#define COUNT     100000000U
-#define COUNT_MOD 10000000U
+#define COUNT 1000000000U
 
 static double _realSums[MAX_THREADS], _ordinalSums[ORDINALS];
-static MT19937 *_randGen[MAX_THREADS];
 static int _nCPUs;
 
+#if USE_MT19937
+  #include "mt19937.h"
+  static MT19937 *_randGen[MAX_THREADS];
+  static double GetRandDouble(int stream) { return Mt19937NextDouble(_randGen[stream]); }
+#else
+  #include "rand48.h"
+  #include "stream48.h"
+  static double GetRandDouble(int stream) { Stream48(stream); return drand48(); }
+#endif
+
 foint ThreadTest(const foint f) {
-    static char _race[COUNT+1]; // who got there first?
+    //static char _race[COUNT+1]; // who got there first?
     assert(f.i < _nCPUs);
-    printf("S%d", f.i+1);
-    unsigned i;
-    for(i=0; i<=COUNT;i++) {
-	double r = Mt19937NextDouble(_randGen[f.i]);
-	unsigned where = Mt19937NextDouble(_randGen[f.i])*ORDINALS;
+    unsigned i, iters=COUNT/_nCPUs;
+    printf(" T%d:%d ", f.i+1, iters); fflush(stdout);
+    for(i=0; i<=iters; i++) {
+	double r = GetRandDouble(f.i);
+	unsigned where = ORDINALS * GetRandDouble(f.i);
 	_ordinalSums[where] += r;
 	_realSums[f.i] += r;
-	if(i % COUNT_MOD == 0) {
+#if 0
+	if(i % iters/10 == 0) {
 	    if(!_race[i]) {_race[i]=f.i+1; printf(" T%d:%uM!", f.i+1,i/1000000);}
 	    else printf(" T%d:%u=%d", f.i+1,i/1000000,_race[i]);
 	    fflush(stdout);
 	}
+#endif
     }
-    printf("\n");
+    //printf("\n");
     return f;
 }
 
@@ -42,10 +52,15 @@ int main(){
     _nCPUs = sysconf(_SC_NPROCESSORS_ONLN);
     assert(0 < _nCPUs && _nCPUs < MAX_THREADS);
     foint threadInput[_nCPUs];
+#if !USE_MT19937
+    Stream48Init(_nCPUs);
+#endif
     for(i=0;i<_nCPUs;i++) {
  	threadInput[i].i=i;
+#if USE_MT19937
 	_randGen[i] = Mt19937Alloc(i);
 	double Mt19937NextDouble(const MT19937 *t);
+#endif
     }
     THREAD_SET *ts = ThreadSetAlloc(ThreadTest, _nCPUs, threadInput);
     ThreadSetRunAll(ts);
