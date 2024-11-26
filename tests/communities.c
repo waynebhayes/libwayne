112441aaeccd393fc48e25d6d12a78b5bd2a7998
Wayne Hayes <whayes@uci.edu>
2024-11-26 12:53:02 -0800

    update communities.c to create "good" communities by BFS

--- tests/communities.c
+++ tests/communities.c
@@ -7,7 +7,7 @@
 /************************** Community routines *******************/
 typedef struct _community {
     int id, n;
-    SET *nodes;
+    SET *nodeSet;
     GRAPH *G; // the graph we came from
 } COMMUNITY;
 
@@ -16,30 +16,42 @@ COMMUNITY *CommunityAlloc(GRAPH *G) {
     C->id = 0; // not sure if this will be handy or not...
     C->n = 0;
     C->G = G;
-    C->nodes = SetAlloc(G->n);
+    C->nodeSet = SetAlloc(G->n);
     return C;
 }
 
+void CommunityFree(COMMUNITY *C) {
+    SetFree(C->nodeSet);
+    Free(C);
+}
+
+// It's not an error to add a member multiple times
 COMMUNITY *CommunityAddNode(COMMUNITY *C, int i) {
-    assert(!SetIn(C->nodes, i));
-    SetAdd(C->nodes, i);
-    C->n++;
+    if(!SetIn(C->nodeSet, i)) {
+	SetAdd(C->nodeSet, i);
+	C->n++;
+	assert(C->n <= C->G->n);
+	assert(SetCardinality(C->nodeSet) == C->n);
+    }
     return C;
 }
 
+// It's not an error to delete a node that's already not there
 COMMUNITY *CommunityDelNode(COMMUNITY *C, int i) {
-    assert(SetIn(C->nodes, i));
-    SetDelete(C->nodes, i);
-    C->n--;
+    if(SetIn(C->nodeSet, i)) {
+	assert(C->n > 0);
+	SetDelete(C->nodeSet, i);
+	C->n--;
+    }
     return C;
 }
 
-int CommunityEdgesIn(COMMUNITY *C) {
+int CommunityEdgeCount(COMMUNITY *C) {
     int *memberList = Calloc(C->n, sizeof(int));
-    int i, j, n = SetToArray(memberList, C->nodes);
+    int i, j, n = SetToArray(memberList, C->nodeSet);
     assert(n == C->n);
     int numEdges=0;
-    printf("CommunityEdgesIn: checking community with %d nodes: ", n);
+    //printf("CommunityEdgeCount: checking community with %d nodes: ", n);
     for(i=0; i<n; i++) {
 	//printf("member %d=%d\n", i, memberList[i]);
 	for(j=i+1; j<n; j++) {
@@ -49,28 +61,49 @@ int CommunityEdgesIn(COMMUNITY *C) {
 	}
     }
     Free(memberList);
-    printf("numEdges %d\n", numEdges);
+    //printf("numEdges %d\n", numEdges);
     return numEdges;
 }
 
-/******************** Sets of Communities (partition) ***********/
+/******************** Sets of non-overlapping Communities (partition) ***********/
 typedef struct _communitySet {
-    unsigned n; // current number of commnuties
+    unsigned n; // current number of non-empty communities
     GRAPH *G; // the graph we came from
     COMMUNITY **C; // array of pointers to COMMUNITY
 } PARTITION;
 
+// Returns an empty partition containing no communities (they're not even allocated)
 PARTITION *PartitionAlloc(GRAPH *G) {
     PARTITION *P = Calloc(sizeof(PARTITION), 1);
-    P->n = G->n;
+    P->G=G;
     P->C = Calloc(sizeof(COMMUNITY**), G->n);
-    int i;
-    for(i=0; i<P->n; i++) P->C[i] = CommunityAlloc(G);
     return P;
 }
+PARTITION *PartitionAddCommunity(PARTITION *P, COMMUNITY *C) {
+    assert(P->n < P->G->n);
+    int i;
+    for(i=0; i<P->G->n; i++) if(!P->C[i]) {P->C[i] = C; P->n++; return P;} // find empty community slot
+    assert(i<P->G->n);
+    return NULL;
+}
+void PartitionFree(PARTITION *P) {
+    int i;
+    for(i=0; i<P->n; i++) if(P->C[i]) CommunityFree(P->C[i]);
+    Free(P->C);
+    Free(P);
+}
 
-double ScorePartition(PARTITION *P) {
-    return 0;
+double ScorePartitionHayes(PARTITION *P) {
+    int i;
+    double score=0;
+    for(i=0; i<P->n; i++) if(P->C[i]) {
+	int n = P->C[i]->n;
+	if(n>1) {
+	    int m = CommunityEdgeCount(P->C[i]);
+	    score += m*m/(n*(n-1)/2.0);
+	}
+    }
+    return score;
 }
 
 int main(int argc, char *argv[])
@@ -92,20 +125,42 @@ int main(int argc, char *argv[])
     PARTITION *P = PartitionAlloc(G);
 
     int numCommunities = 10; // communities numbered 0 through numCommunities-1 inclusive
-    printf("Creating %d communities\n", numCommunities);
+    printf("Creating %d random communities: ", numCommunities);
+    for(i=0; i<numCommunities; i++) PartitionAddCommunity(P, CommunityAlloc(G));
 
-    P->n = numCommunities;
     for(i=0; i<G->n; i++) {
 	int which = (int)(drand48() * numCommunities);
 	CommunityAddNode(P->C[which], i);
-	// printf("%d->%d, ", i, which);
+	//printf("%d->%d, ", i, which);
     }
-    //puts("");
 
-    for(i=0; i<numCommunities; i++) CommunityEdgesIn(P->C[i]);
-    puts("");
-    double s = ScorePartition(P);
-    printf("Partition score is %g\n", s);
+    double s = ScorePartitionHayes(P);
+    printf("score = %g\n", s);
+    PartitionFree(P);
+
+    printf("BFS-based communities: ");
+    P = PartitionAlloc(G);
+    SET *nodesUsed=SetAlloc(G->n); // cumulative set of nodes that have been put into a partition
+
+    int nodeArray[G->n], distArray[G->n];
+    while(SetCardinality(nodesUsed) < G->n) {
+	int seed;
+	do { seed = (int)(drand48() * G->n); }
+	    while(SetIn(nodesUsed, seed));
+	int numAdded=0, distance = 4; // should be far enough
+	int n=GraphBFS(G, seed, distance, nodeArray, distArray); // list of nodes within "distance" of seed
+	//printf("BFS(%d[%d])=%d", seed, G->degree[seed], n);
+	assert(n>0 && nodeArray[0]==seed && distArray[seed]==0);
+	COMMUNITY *C = CommunityAlloc(G);
+	for(i=0; i<n; i++) if(!SetIn(nodesUsed,nodeArray[i]) || drand48() > 0.5) {
+	    SetAdd(nodesUsed,nodeArray[i]); CommunityAddNode(C,nodeArray[i]); ++numAdded;
+	}
+	printf("%d ", numAdded); fflush(stdout);
+	assert(C->n >0 && C->n < G->n && C->n==numAdded);
+	PartitionAddCommunity(P, C);
+    }
+    printf("\n%d communities, score = %g\n", P->n, ScorePartitionHayes(P));
+    PartitionFree(P);
 
     return 0;
 }
